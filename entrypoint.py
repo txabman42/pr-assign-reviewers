7e8338e6532dd33e92f78fb23074b1660d558488
xabierMartinez42 <xabier.martinez@taiger.com>
2020-03-21 20:51:03 +0100

    Retrieve PR info

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,30 @@
 import os
+import sys
+import json
+import requests
 
-print('XMB - This is working!!!')
-print(os.environ['GITHUB_TOKEN'])
-print(os.environ['GITHUB_REPOSITORY'])
-print(os.environ['GITHUB_EVENT_PATH'])
+if (os.environ['GITHUB_REPOSITORY'] == null) {
+    sys.('The env variable GITHUB_REPOSITORY is required')
+}
+
+if (os.environ['GITHUB_EVENT_PATH'] == null) {
+    sys.('The env variable GITHUB_EVENT_PATH is required')
+}
+
+URI = 'https://api.github.com'
+API_HEADER = 'Accept: application/vnd.github.v3+json'
+AUTH_HEADER = 'Authorization: token' + os.environ['GITHUB_TOKEN']
+
+event_path = json.loads(os.environ['GITHUB_EVENT_PATH'])
+print(event_path)
+number = event_path[".pull_request.number"]
+print('Number: ' + number)
+
+print('GitHub event')
+print os.environ['GITHUB_EVENT_PATH']
+
+def get_pr_info():
+    api_url = URI + '/repos/' + os.environ['GITHUB_REPOSITORY'] + '/pulls/'
+    headers = {API_HEADER, AUTH_HEADER}
+    response = requests.get(api_url, headers=headers)
+    print(response)
